f3a04108cf784a018f506e83e4d4f742788e6a2c
Ericko Wicaksono <ericko.wicaksono@gmail.com>
2018-03-25 20:16:52 +0700

    add template

---
+++ src/views/EditProfile.vue
@@ -0,0 +1,110 @@
+<template>
+    <div class=" edit-profil animated fadeIn">
+      <div class="row">
+        <div class="col-sm-12 col-md-8 col-lg-8">
+            <div class="card">
+                <div class="card-header"><div><strong>Ubah Data Profil</strong></div></div>
+                <div class="card-body">
+                    <div class="row">
+                            <div class="col-sm-12">
+                                <fieldset id="__BVID__190" role="group" class="b-form-group form-group">
+                                    <div role="group" class="">
+                                        <label for="name">Nama</label> 
+                                        <input id="name" type="text" placeholder="Masukkan nama anda" class="form-control">
+                                    </div>
+                                </fieldset>
+                            </div>
+                        </div> 
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <fieldset id="__BVID__191" role="group" class="b-form-group form-group">
+                                    <div role="group" class="">
+                                        <label for="nim">NIM</label> 
+                                        <input id="nim" type="text" placeholder="Masukkan NIM" class="form-control">
+                                    </div>
+                                </fieldset>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <fieldset id="__BVID__192" role="group" class="b-form-group form-group">
+                                    <div role="group" class="">
+                                        <label for="jurusan">Jurusan</label> 
+                                        <input id="jurusan" type="text" placeholder="Masukkan Jurusan" class="form-control">
+                                    </div>
+                                </fieldset>
+                            </div>
+                        </div>
+                        <div class="row text-center">
+                            <div class="col-sm-12 ">
+                                <div class="form-group form-actions ">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-4 col-lg-4">
+          <div class="card">
+                <div class="card-header"><div><strong>Ganti Password</strong></div></div>
+                <div class="card-body">
+                    <div class="row">
+                            <div class="col-sm-12">
+                                <fieldset id="__BVID__200" role="group" class="b-form-group form-group">
+                                    <div role="group" class="">
+                                        <label for="oldPasword">Password lama</label> 
+                                        <input id="oldPasword" type="password" placeholder="Masukkan nama anda" class="form-control">
+                                    </div>
+                                </fieldset>
+                            </div>
+                        </div> 
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <fieldset id="__BVID__201" role="group" class="b-form-group form-group">
+                                    <div role="group" class="">
+                                        <label for="newPassword">Password baru</label> 
+                                        <input id="newPassword" type="password" placeholder="Masukkan NIM" class="form-control">
+                                    </div>
+                                </fieldset>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <fieldset id="__BVID__202" role="group" class="b-form-group form-group">
+                                    <div role="group" class="">
+                                        <label for="confirmPassword">Konfirmasi password</label> 
+                                        <input id="confirmPassword" type="password" placeholder="Masukkan Jurusan" class="form-control">
+                                    </div>
+                                </fieldset>
+                            </div>
+                        </div>
+                        <div class="row text-center">
+                            <div class="col-sm-12 ">
+                                <div class="form-group form-actions ">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'edit-profil',
+  mounted () {
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
---
+++ src/views/public/Register.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="app flex-row align-items-center">
+    <div class="container">
+      <b-row class="justify-content-center">
+        <b-col md="6">
+            <div class="card-group">
+                <div class="card p-4">
+                    <div class="card-body p-4"> 
+                        <h1>Register</h1>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <fieldset id="__BVID__261" role="group" class="b-form-group form-group">
+                                    <div role="group" class="">
+                                        <label for="name">Nama</label> 
+                                        <input id="name" type="text" placeholder="Masukkan nama anda" class="form-control">
+                                    </div>
+                                </fieldset>
+                            </div>
+                        </div> 
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <fieldset id="__BVID__262" role="group" class="b-form-group form-group">
+                                    <div role="group" class="">
+                                        <label for="nim">NIM</label> 
+                                        <input id="nim" type="text" placeholder="Masukkan NIM" class="form-control">
+                                    </div>
+                                </fieldset>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <fieldset id="__BVID__263" role="group" class="b-form-group form-group">
+                                    <div role="group" class="">
+                                        <label for="jurusan">Jurusan</label> 
+                                        <input id="jurusan" type="text" placeholder="Masukkan Jurusan" class="form-control">
+                                    </div>
+                                </fieldset>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <fieldset id="__BVID__264" role="group" class="b-form-group form-group">
+                                    <div role="group" class="">
+                                        <label for="email">E-mail</label> 
+                                        <input id="email" type="email" placeholder="Masukkan e-mail" class="form-control">
+                                    </div>
+                                </fieldset>
+                            </div>
+                        </div>
+                        <div class="row text-center">
+                            <div class="col-sm-12 ">
+                                <div class="form-group form-actions ">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </b-col>
+      </b-row>
+      
+    </div>
+  </div>
+    
+</template>
+
+<script>
+export default {
+  name: 'register',
+  mounted () {
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
